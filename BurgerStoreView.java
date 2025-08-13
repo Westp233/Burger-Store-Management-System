52ad803b12c24ba5e441fb06cf3c07cac4c5d678
Westp233 <Ws15011027055@outlook.com>
2025-08-13 16:15:35 +1000

    Unfinished Login Frame

--- BurgerStoreView.java
+++ BurgerStoreView.java
@@ -1,9 +1,12 @@
 import javafx.geometry.Pos;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 public class BurgerStoreView {
@@ -11,6 +14,8 @@ public class BurgerStoreView {
     private Label welcomLabel;
     private Button startBtn;
     private Button staffLoginBtn;
+    private Button loginBtn;
+    private Button closeBtn;
 
     private BurgerStoreController controller;
     private BurgerStoreModel model;
@@ -60,7 +65,55 @@ public class BurgerStoreView {
 
     }
 
-    private void createLoginMenu() {
+    private void createLoginMenu() {// This is the login window for the staff
+        Stage stage = new Stage();
+        stage.initOwner(primaryStage);
+        stage.initModality(Modality.APPLICATION_MODAL);
+
+        TextField idField = new TextField();
+        HBox idRow = new HBox(5, new Label("StaffID: "), idField);
+
+        TextField passWordField = new TextField();
+        HBox passWordRow = new HBox(5, new Label("Password: "), passWordField);
+
+        loginBtn = new Button("Login");
+        // Staff staff = model.staffLogin(idField.getText().trim(), passWordField.getText().trim());
+        // loginBtn.setOnAction(event -> {
+        //     if (!staff == null) {
+        //         if (staff.getManagerPermission()) {
+        //             createManagerMenu();
+        //         } else {
+        //             createStaffMenu();
+        //         }
+        //     } else {
+        //         createErrorPage();
+        //     }
+        // });
+
+        closeBtn = new Button("Close");
+        closeBtn.setOnAction(event -> stage.close());
+
+        HBox btnRow = new HBox(10, loginBtn, closeBtn);
+        btnRow.setAlignment(Pos.CENTER);
+
+        VBox root = new VBox(5, idRow, passWordRow, btnRow);
+        root.setAlignment(Pos.CENTER);
+
+        Scene loginScene = new Scene(root, 400, 200);
+
+        stage.setScene(loginScene);
+        stage.show();
+    }
+
+    private void createManagerMenu() {
+
+    }
+
+    private void createStaffMenu() {
+
+    }
+
+    private void createErrorPage() {
 
     }
 }
